130d504e3bbbae8e134673c7ab98011689ea7bc0
Seth  Koch <sethkoch@Seths-MacBook-Pro.local>
2018-03-21 08:59:59 -0700

    first push

---
+++ README.md
@@ -0,0 +1,7 @@
+#Learn JavaScript part 13
+
+Functions can return functions, and hey, those returned functions can be immediatly invoked!  
+
+Each button on the web page should pop up an alert box calling out the danger.  It's not working because you need to complete the code.  Add your own custom warning for each button. 
+
+If you have any questions, please ask on the chat box at https://www.namelesshacker.com
---
+++ js.js
@@ -0,0 +1,21 @@
+var warning = function() {
+  return function(message) {
+    alert(message);
+  }
+}
+
+document.getElementById('zombies').addEventListener('click', function() {
+  //write code here
+});
+document.getElementById('rabidBats').addEventListener('click', function() {
+  //write code here
+});
+document.getElementById('seaMonsters').addEventListener('click', function() {
+  //write code here
+});
+document.getElementById('monkeysWithGuns').addEventListener('click', function() {
+  //write code here
+});
+document.getElementById('badPeople').addEventListener('click', function() {
+  //write code here
+});
